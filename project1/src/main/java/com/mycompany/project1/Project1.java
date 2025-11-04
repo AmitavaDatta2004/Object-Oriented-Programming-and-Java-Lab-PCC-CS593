ed32d5f351fa3cc2debe636b714a629f981b7298
Amitava Datta <dattaarup2004@gmail.com>
2025-11-04 16:18:06 +0530

    Project created

---
+++ project1/src/main/java/com/mycompany/project1/Project1.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.project1;
+
+/**
+ *
+ * @author AMITAVA
+ */
+
+
+class Pen {
+    String color;
+    String type; //ballpoint,gel;
+    
+    public void write(){
+        System.out.println("Write something");
+    }
+}
+
+
+public class Project1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
