adb4dd0782757d85d014c2089d79395ff39fdf2c
Amitava Datta <dattaarup2004@gmail.com>
2025-11-04 17:12:51 +0530

    Project created

--- project1/src/main/java/com/mycompany/project1/Project1.java
+++ project1/src/main/java/com/mycompany/project1/Project1.java
@@ -17,12 +17,33 @@ class Pen {
     public void write(){
         System.out.println("Write something");
     }
+    
+    public void printColor(){
+        System.out.println(color);
+    }
+//    public void read(){
+//        System.out.println("");
+//    }
 }
+
 
 
 public class Project1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        //System.out.println("Hello World!");
+        Pen pen1 = new Pen();
+        pen1.color="blue";
+        pen1.type="gel";
+        
+        pen1.write();
+        
+        
+        Pen pen2=new Pen();
+        pen2.color="red";
+        pen2.type="ballpoint";
+        
+        pen1.printColor();
+        pen2.printColor();
     }
 }
