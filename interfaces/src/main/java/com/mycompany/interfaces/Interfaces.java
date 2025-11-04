bce5110fcc8272580dbccda252f610f804e92a04
Amitava Datta <dattaarup2004@gmail.com>
2025-11-05 02:04:52 +0530

    practice

---
+++ interfaces/src/main/java/com/mycompany/interfaces/Interfaces.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.interfaces;
+
+/**
+ *
+ * @author AMITAVA
+ */
+
+import java.util.Scanner;
+import bank.*;
+
+public class Interfaces {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
