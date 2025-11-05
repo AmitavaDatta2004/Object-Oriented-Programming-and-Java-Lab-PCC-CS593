5c7c6d58dc61b886e1a3c5d5706c4c6f5a05a74f
Amitava Datta <dattaarup2004@gmail.com>
2025-11-05 20:59:01 +0530

    practice

---
+++ excceptions/src/main/java/com/mycompany/excceptions/Excceptions.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.excceptions;
+
+/**
+ *
+ * @author AMITAVA
+ */
+public class Excceptions {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        int[] numerators = {100,253,365,852};
+        int[] denominators = {2,5,6,6};
+        
+        try {
+            for(int i=0;i<numerators.length+5;i++){
+                System.out.println(numerators[i]/denominators[i]);
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+            System.out.println("Divided by 0");
+        }
+        
+
+    }
+}
